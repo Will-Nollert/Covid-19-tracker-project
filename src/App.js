74d8b92f9e1691115e2a621323969b9f90663616
William Nollert <will.nollert@gmail.com>
2022-01-27 16:10:24 -0600

    feat: added country dropdown list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>covid19 tracker</h1>
+      <div className="app_header">
+        <h1>covid19 tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
